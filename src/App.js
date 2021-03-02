4938c43c002b66e9782793c780f06d14b213043c
AydenZK <aydenkhalil619@gmail.com>
2021-03-03 00:04:16 +1100

    merged src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
+import Job from './Job/Job';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    jobs: [
+      {
+        position: 'Software Engineer Intern', 
+        employer:'NAB', 
+        startYear:'2021'
+      },
+      {
+        position:'Industry Manager/RL Project Member', 
+        employer:'Monash DeepNeuron', 
+        startYear:'2020'
+      }
+    ]
+  }
+  
+  x = 0;
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ayden Khalil</h1>
+        <button onClick={this.x += 1}>Switch Job</button>
+        < Person name='Ayden' age='20'/> 
+        <br/>
+        < Job position={this.state.jobs[this.x].position} employer={this.state.jobs[this.x].employer} startYear={this.state.jobs[this.x].startYear}/>
+      </div>
+    );
+    // < Person /> is a self closing tag
+    //return React.createElement('div', {className: 'App'}, React.createElement('hi', null, 'Does rhis woork'))
+  }
 }
 
 export default App;
